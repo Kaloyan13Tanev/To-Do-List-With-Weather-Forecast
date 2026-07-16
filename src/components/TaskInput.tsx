4595fd9d610446945d8ff4a7db0f193e8c66c301
Kaloyan13Tanev <kikoran.kt@gmail.com>
2026-07-16 09:45:34 +0300

    Add TaskInput component

---
+++ src/components/TaskInput.tsx
@@ -0,0 +1,16 @@
+function TaskInput() {
+  return (
+    <div className="mb-3">
+      <label htmlFor="exampleFormControlTextarea1" className="form-label">
+        Example textarea
+      </label>
+      <textarea
+        className="form-control"
+        id="exampleFormControlTextarea1"
+        rows={3}
+      ></textarea>
+    </div>
+  );
+}
+
+export default TaskInput;
