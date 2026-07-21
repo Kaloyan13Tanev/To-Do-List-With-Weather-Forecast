58f52220f8a0533ac050d573e27f4abff04270cc
Kaloyan13Tanev <kikoran.kt@gmail.com>
2026-07-21 16:57:55 +0300

    Add task repository
    TaskRepository defines the persistence contract for Task entities

---
+++ backend/todolist/src/main/java/bg/sofia/elando/todolist/repository/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package bg.sofia.elando.todolist.repository.repository;
+
+import bg.sofia.elando.todolist.repository.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+}
