0b9df7abd1ffdfb170749670130e46ce51f17aff
Kaloyan13Tanev <kikoran.kt@gmail.com>
2026-07-22 13:50:00 +0300

    Add repository notation to TaskRepository

--- backend/todolist/src/main/java/bg/sofia/elando/todolist/repository/TaskRepository.java
+++ backend/todolist/src/main/java/bg/sofia/elando/todolist/repository/TaskRepository.java
@@ -3,13 +3,17 @@ package bg.sofia.elando.todolist.repository;
 import bg.sofia.elando.todolist.entity.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface TaskRepository extends JpaRepository<TaskEntity, UUID> {
 
     @Query("select t from TaskEntity t order by t.position desc")
     List<TaskEntity> getTasks();
+
+
 
 }
